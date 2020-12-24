cfff8180319fed91e69d8c4f29649cd3ad33f76a
Marat Fakhrutdinov <maratfakhrutdinov2019@gmail.com>
2020-12-24 15:24:24 +0300

    add comments

--- js/entry-form.js
+++ js/entry-form.js
@@ -1,20 +1,22 @@
-/*TODO: comments, refactoring*/
+/*TODO: more comments, refactoring*/
+//PS. Некоторые простые случаи валидации зашил в html, e.g. для ДНЯ рождения: value min="1" max="31"
 
 const MAX_FIRST_NAME = 10;
 const MAX_LAST_NAME = 15;
 const MSG_VALUE_TOO_LONG = "Значение слишком длинное";
 const MSG_VALUE_EMPTY = "Значение не может быть пустым";
 const MSG_VALUE_INVALID = "Неверное значение";
-
 const DAYS_IN_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 const firstName = document.getElementById("rf-input-first-name");
+//проверяем уход фокуса с инпута имени и сообщаем об ошибке, если оставлен пустым
 firstName.addEventListener("focusout", function (event) {
     let val = (String)(firstName.value);
     if (val.length === 0) {
         firstName.setCustomValidity(MSG_VALUE_EMPTY);
     }
 })
+//валидируем введённое имя
 firstName.addEventListener("input", function (event) {
     let val = (String)(firstName.value);
     if (val.length > MAX_FIRST_NAME) {
@@ -26,12 +28,14 @@ firstName.addEventListener("input", function (event) {
 })
 
 const lastName = document.getElementById("rf-input-last-name");
+//проверяем уход фокуса с инпута фамилии и сообщаем об ошибке, если оставлен пустым
 lastName.addEventListener("focusout", function (event) {
     let val = (String)(lastName.value);
     if (val.length === 0) {
         lastName.setCustomValidity(MSG_VALUE_EMPTY);
     }
 })
+//валидируем введённую фамилию
 lastName.addEventListener("input", function (event) {
     let val = (String)(lastName.value);
     if (val.length > MAX_LAST_NAME) {
@@ -44,12 +48,14 @@ lastName.addEventListener("input", function (event) {
 })
 
 const birthDay = document.getElementById("rf-birth-day");
-birthDay.addEventListener("focusout", (event) => validateBirthDay());
+birthDay.addEventListener("focusout", (event) => validateBirthDay())
+//валидируем введённый ДЕНЬ рождения и проверяем консистентность получившейся даты рождения
 birthDay.addEventListener("input", function (event) {
     if (validateBirthDay()) {
         validateBirthDate();
     }
 })
+
 
 function validateBirthDay() {
     let val = (String)(birthDay.value);
@@ -61,10 +67,12 @@ function validateBirthDay() {
     return true;
 }
 
+//Достаём Месяц рождения. P.S. у селекта месяцев стоит onchange="validateBirthDate();"
 const birthMonth = document.getElementById("rf-birth-month");
 
 const birthYear = document.getElementById("rf-birth-year");
 birthYear.addEventListener("focusout", (event) => validateBirthYear())
+//валидируем введённый ГОД рождения и проверяем консистентность получившейся даты рождения
 birthYear.addEventListener("input", function (event) {
     if (validateBirthYear()) {
         validateBirthDate();
@@ -81,6 +89,7 @@ function validateBirthYear() {
     return true;
 }
 
+//Валидируем введённые данные как дату рождения
 function validateBirthDate() {
     try {
         var day = parseInt((String)(birthDay.value));
@@ -111,6 +120,7 @@ function validateBirthDate() {
     return true;
 }
 
+//Проверяем дату рождения на консистентность
 function isBirthDateConsistant(year, month, day) {
     let daysInMonth = DAYS_IN_MONTH[month - 1];
     if (day > daysInMonth) {
@@ -125,6 +135,7 @@ function isBirthDateConsistant(year, month, day) {
     return true;
 }
 
+//Проверка на високосность года
 function isLeapYear(year) { 
     if (year % 4 === 0) {
         if (year % 100 === 0) {
@@ -137,12 +148,14 @@ function isLeapYear(year) {
     return false;
 }
 
+//Ставим всю дату рождения в невалидное состояние
 function invalidBirth(msg) {
     birthYear.setCustomValidity(msg);
     birthMonth.setCustomValidity(msg);
     birthDay.setCustomValidity(msg);
 }
 
+//Ставим всю дату рождения в валидное состояние
 function validBirth() {
     birthYear.setCustomValidity("");
     birthMonth.setCustomValidity("");
