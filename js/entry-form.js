58953eb8e54ab02752e5eab57b0921339d6b5d84
Marat Fakhrutdinov <maratfakhrutdinov2019@gmail.com>
2020-12-26 21:54:29 +0300

    refactor js

--- js/entry-form.js
+++ js/entry-form.js
@@ -1,131 +1,109 @@
-/*TODO: more comments, refactoring*/
-//PS. Некоторые простые случаи валидации зашил в html, e.g. для ДНЯ рождения: value min="1" max="31"
-
-const MAX_FIRST_NAME = 10;
-const MAX_LAST_NAME = 15;
 const MSG_VALUE_TOO_LONG = "Значение слишком длинное";
 const MSG_VALUE_EMPTY = "Значение не может быть пустым";
 const MSG_VALUE_INVALID = "Неверное значение";
+// связка индексов месяцев с количеством дней в месяце, где: 0 - январь, 1 - февраль, ..., 11 - декабрь
 const DAYS_IN_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-const firstName = document.getElementById("rf-input-first-name");
-//проверяем уход фокуса с инпута имени и сообщаем об ошибке, если оставлен пустым
-firstName.addEventListener("focusout", function (event) {
-    let val = (String)(firstName.value);
-    if (val.length === 0) {
-        firstName.setCustomValidity(MSG_VALUE_EMPTY);
-    }
-})
-//валидируем введённое имя
-firstName.addEventListener("input", function (event) {
-    let val = (String)(firstName.value);
-    if (val.length > MAX_FIRST_NAME) {
-        firstName.setCustomValidity(MSG_VALUE_TOO_LONG);
-    }
-    else {
-        firstName.setCustomValidity("");
-    }
-})
-
-const lastName = document.getElementById("rf-input-last-name");
-//проверяем уход фокуса с инпута фамилии и сообщаем об ошибке, если оставлен пустым
-lastName.addEventListener("focusout", function (event) {
-    let val = (String)(lastName.value);
-    if (val.length === 0) {
-        lastName.setCustomValidity(MSG_VALUE_EMPTY);
-    }
-})
-//валидируем введённую фамилию
-lastName.addEventListener("input", function (event) {
-    let val = (String)(lastName.value);
-    if (val.length > MAX_LAST_NAME) {
-        lastName.setCustomValidity(MSG_VALUE_TOO_LONG);
-        return;
-    }
-    else {
-        lastName.setCustomValidity("");
-    }
-})
-
-const birthDay = document.getElementById("rf-birth-day");
-birthDay.addEventListener("focusout", (event) => validateBirthDay())
-//валидируем введённый ДЕНЬ рождения и проверяем консистентность получившейся даты рождения
-birthDay.addEventListener("input", function (event) {
-    if (validateBirthDay()) {
-        validateBirthDate();
-    }
-})
-
-
-function validateBirthDay() {
-    let val = (String)(birthDay.value);
-    if (val.length === 0) {
-        birthDay.setCustomValidity(MSG_VALUE_EMPTY);
-        return false;
-    }
-
-    return true;
+const Name = {
+    all: document.getElementsByClassName("rf-name-item"),
+    first: document.getElementById("rf-input-first-name"),
+    last: document.getElementById("rf-input-last-name"),
 }
 
-//Достаём Месяц рождения. P.S. у селекта месяцев стоит onchange="validateBirthDate();"
-const birthMonth = document.getElementById("rf-birth-month");
-
-const birthYear = document.getElementById("rf-birth-year");
-birthYear.addEventListener("focusout", (event) => validateBirthYear())
-//валидируем введённый ГОД рождения и проверяем консистентность получившейся даты рождения
-birthYear.addEventListener("input", function (event) {
-    if (validateBirthYear()) {
-        validateBirthDate();
-    }
-})
-
-function validateBirthYear() {
-    let val = (String)(birthYear.value);
-    if (val.length === 0) {
-        birthYear.setCustomValidity(MSG_VALUE_EMPTY);
-        return false;
-    }
-
-    return true;
+const BirthDate = {
+    all: document.getElementsByClassName("rf-birth-item"),
+    day: document.getElementById("rf-birth-day"),
+    month: document.getElementById("rf-birth-month"),
+    year: document.getElementById("rf-birth-year")
 }
 
-//Валидируем введённые данные как дату рождения
+//Валидация формы, true - валидна, false - невалидна
+function validateForm() {
+    return validateBirthDate() && validateName();
+}
+
+//Валидируем дату рождения, true - валидна, false - невалидна
 function validateBirthDate() {
+    setBirthValidState();
+    //Проверяем каждый элемент даты рождения на заполненность. Помечаем невалидными только пустые элементы
+    let noEmpty = true;
+    for (let i = 0; i < BirthDate.all.length; i++) {
+        let birthElem = BirthDate.all[i]
+
+        //noEmpty &&= validateNotEmpty(BirthDate.all[i]); Оператор ленивый - вычислять второй операнд не будет, если noEmpty уже false
+        let e = validateNotEmpty(birthElem);
+        noEmpty &&= e;
+    }
+
+    //Если есть незаполненные элементы, то дальнейшие проверки лишены смысла по ТЗ
+    if (!noEmpty) {
+        return false;
+    }
+    
+    //Проверяем, что введённые данные являются валидными цифрами
     try {
-        var day = parseInt((String)(birthDay.value));
-        var month = parseInt(birthMonth.value);
-        var year = parseInt((String)(birthYear.value));
+        var day = parseInt(BirthDate.day.value);
+        var month = parseInt(BirthDate.month.value);
+        var year = parseInt(BirthDate.year.value);
     }
     catch (e) {
-        console.log(e);
-        invalidBirth(MSG_VALUE_INVALID);
+        console.error(e);
+        setBirthValidState(MSG_VALUE_INVALID);
         return false;
     }
 
-    if (!isBirthDateConsistant(year, month, day)) {
-        invalidBirth(MSG_VALUE_INVALID);
+    //Проверяем консистентность данных, ведь нам не нужно 31 февраля, верно ? :)
+    if (!isBirthDateConsistent(year, month, day)) {
+        setBirthValidState(MSG_VALUE_INVALID);
         return false;
     }
 
     let birthDate = new Date(year, month, day);
     let currentDate = new Date();
 
-    //Проводим наивную проверку, что birthDate не в будущем относительно даты сервера
+    //Проводим наивную проверку, что birthDate не в будущем относительно даты окружения
+    //На самом деле здесь должна быть проверка на соответствие ДР пользователя политике компании по минимальному возрасту пользователей
     if (birthDate > currentDate) {
-        invalidBirth(MSG_VALUE_INVALID);
+        setBirthValidState(MSG_VALUE_INVALID);
         return false;
     }
 
-    validBirth();
+    //Ура! Дата рождения прошла проверки на валидность!
+    setBirthValidState();
     return true;
 }
 
-//Проверяем дату рождения на консистентность
-function isBirthDateConsistant(year, month, day) {
+//Валидация элементов имени, true валидны, false - невалидны
+function validateName() {
+    let noEmpty = true;
+    for (let i = 0; i < Name.all.length; i++) {
+        let nameElem = Name.all[i];
+
+        //noEmpty &&= validateNotEmpty(nameElem); Оператор ленивый - вычислять второй операнд не будет, если noEmpty уже false
+        let e = validateNotEmpty(nameElem);
+        noEmpty &&= e;
+    }
+
+    return noEmpty;
+}
+
+//Проверка значения элемента на заполненность, true - заполнен, false - незаполнен
+function validateNotEmpty(elem) {
+    if (elem.value.length === 0) {
+        elem.setCustomValidity(MSG_VALUE_EMPTY);
+        return false;
+    }
+    else {
+        elem.setCustomValidity("");
+        return true;
+    }
+}
+
+//Проверка даты рождения на консистентность
+function isBirthDateConsistent(year, month, day) {
     let daysInMonth = DAYS_IN_MONTH[month];
     if (day > daysInMonth) {
-        //февраль
-        if (day === 29) {
+        if (day === 29) { //Февраль
             return isLeapYear(year)
         }
 
@@ -135,8 +113,8 @@ function isBirthDateConsistant(year, month, day) {
     return true;
 }
 
-//Проверка на високосность года
-function isLeapYear(year) { 
+//Проверка на високосность года. Правила високосности года можете загуглить :)
+function isLeapYear(year) {
     if (year % 4 === 0) {
         if (year % 100 === 0) {
             return year % 400 === 0 ? true : false;
@@ -144,20 +122,14 @@ function isLeapYear(year) {
 
         return true;
     }
-    
+
     return false;
 }
 
-//Ставим всю дату рождения в невалидное состояние
-function invalidBirth(msg) {
-    birthYear.setCustomValidity(msg);
-    birthMonth.setCustomValidity(msg);
-    birthDay.setCustomValidity(msg);
-}
-
-//Ставим всю дату рождения в валидное состояние
-function validBirth() {
-    birthYear.setCustomValidity("");
-    birthMonth.setCustomValidity("");
-    birthDay.setCustomValidity("");
+//Выставляет всей дате рождения определённое состояние валидности
+function setBirthValidState(msg = "") {
+    for (let i = 0; i < BirthDate.all.length; i++) {
+        let birthElem = BirthDate.all[i];
+        birthElem.setCustomValidity(msg);
+    }
 }
